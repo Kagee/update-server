782e26338e37e335444716b710b11967574999c5
Anders Einar Hilden <hildenae@gmail.com>
2012-08-07 20:52:10 +0200

    Reformat code, functions

--- install-server.sh
+++ install-server.sh
@@ -1,88 +1,94 @@
 #! /bin/sh
-
 set -e
-
 DBPWD=$(head -c 32 /dev/urandom | base64)
 RANDOMSALT=$(head -c 32 /dev/urandom | base64)
 BASE="/root/FixMyStreet"
 
-if [ "$(grep 'http://ftp.no.debian.org/debian testing' /etc/apt/sources.list | wc -l)" -ne "1" ]
-then
-	echo "\ndeb http://ftp.no.debian.org/debian testing main contrib non-free" >> /etc/apt/sources.list
-        echo "\ndeb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list
-fi
+add-apt-repo() {
+	if [ "$(grep 'http://ftp.no.debian.org/debian testing' /etc/apt/sources.list | wc -l)" -ne "1" ]
+	then
+		echo "\ndeb http://ftp.no.debian.org/debian testing main contrib non-free" >> /etc/apt/sources.list
+	        echo "\ndeb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list
+	fi
+	if [ "$(grep '^Pin: release a=testing' /etc/apt/sources.list | wc -l)" -ne "1" ]
+	then
+		echo "Package: *\nPin: release a=stable\nPin-Priority: 600\n" >> /etc/apt/preferences
+		echo "Package: *\nPin: release a=squeeze-backports\nPin-Priority: 200\n" >> /etc/apt/preferences
+		echo "Package: *\nPin: release a=testing\nPin-Priority: 50\n" >> /etc/apt/preferences
+	fi
+	apt-get update
+}
 
-if [ "$(grep '^Pin: release a=testing' /etc/apt/sources.list | wc -l)" -ne "1" ]
-then
-    echo "Package: *\nPin: release a=squeeze-backports\nPin-Priority: 200\n" >> /etc/apt/preferences
-    echo "Package: *\nPin: release a=testing\nPin-Priority: 50\n" >> /etc/apt/preferences
-fi
+setup-folders-git-checkout() {
+	apt-get install -y git
+	if [ ! -d $BASE ]; then
+	    mkdir $BASE
+	fi
+	cd $BASE
+	if [ ! -d "fixmystreet" ]; then
+	    git clone --recursive https://github.com/mysociety/fixmystreet.git
+	else
+	    echo "Will assume that code is already cloned"
+	fi
+	cd fixmystreet
+}
 
-apt-get update
+generate-locales() {
+	# Generate required locales
+	if [ "$(locale -a | grep '^en_GB.utf8$' | wc -l)" -eq "1" ]
+	then
+	    echo "en_GB.utf8 activated and generated"
+	else
+	    echo "en_GB.utf8 not generated"
+	    if [ "$(grep '^en_GB.UTF-8 UTF-8' /etc/locale.gen | wc -l)" -eq "1" ]
+	    then
+	        echo "'en_GB.UTF-8 UTF-8' already in /etc/locale.gen we will only generate"
+	    else
+		echo "Appending 'en_GB.UTF-8 UTF-8' and 'cy_GB.UTF-8 UTF-8'"
+		echo "to /etc/locale.gen for generation"
+		echo "\nen_GB.UTF-8 UTF-8\ncy_GB.UTF-8 UTF-8" >> /etc/locale.gen
+	    fi
+	    echo "Generating new locales"
+    	    locale-gen
+	fi
+}
 
-apt-get install -y git
+install-and-setup-psql() {
+	apt-get -y install postgresql-8.4
 
-if [ ! -d $BASE ]; then
-    mkdir $BASE
-fi
+	#TODO: Remove when script complete
+	echo "DROP DATABASE fms; DROP USER FMS;" | su postgres -c "psql --echo-all"
 
-cd $BASE
+	echo "CREATE USER fms WITH PASSWORD '$DBPWD'; CREATE DATABASE fms WITH OWNER fms;" | su postgres -c "psql --echo-all"
 
-if [ ! -d "fixmystreet" ]; then
-    git clone --recursive https://github.com/mysociety/fixmystreet.git
-else
-    echo "Will assume that code is already cloned"
-fi
+	if ! echo "\q" | psql -d fms -U fms
+	then
+		# psql: FATAL:  Ident authentication failed for user "fms"
+		echo "Adding 'local fms fms trust' to /etc/postgresql/8.4/main/pg_hba.conf"
+		cp /etc/postgresql/8.4/main/pg_hba.conf /etc/postgresql/8.4/main/pg_hba.conf.fms
+		echo "local fms fms trust\n" > /etc/postgresql/8.4/main/pg_hba.conf
+		cat /etc/postgresql/8.4/main/pg_hba.conf.fms >> /etc/postgresql/8.4/main/pg_hba.conf
+		rm /etc/postgresql/8.4/main/pg_hba.conf.fms
+		/etc/init.d/postgresql restart
+	else
+		echo "Can connect to psql using fms"
+		exit 1;
+	fi
 
-cd fixmystreet
+	echo "CREATE LANGUAGE plpgsql;" | psql -d fms -U fms
 
-# Generate required locales
-if [ "$(locale -a | grep '^en_GB.utf8$' | wc -l)" -eq "1" ]
-then
-    echo "en_GB.utf8 activated and generated"
-else
-    echo "en_GB.utf8 not generated"
-    if [ "$(grep '^en_GB.UTF-8 UTF-8' /etc/locale.gen | wc -l)" -eq "1" ]
-    then
-        echo "'en_GB.UTF-8 UTF-8' already in /etc/locale.gen we will only generate"
-    else
-        echo "Appending 'en_GB.UTF-8 UTF-8' and 'cy_GB.UTF-8 UTF-8'"
-	echo "to /etc/locale.gen for generation"
-        echo "\nen_GB.UTF-8 UTF-8\ncy_GB.UTF-8 UTF-8" >> /etc/locale.gen
-    fi
-    echo "Generating new locales"
-    locale-gen
-fi
+	echo "Importing schemas"
+	psql -d fms -U fms < db/schema.sql
+	psql -d fms -U fms < db/alert_types.sql
 
-apt-get -y install postgresql-8.4
+	echo "Inserting secret"
+	echo "INSERT INTO secret VALUES ('$RANDOMSALT');" | psql -d fms -U fms
+}
 
-#TODO: Remove when script complete
-echo "DROP DATABASE fms; DROP USER FMS;" | su postgres -c "psql --echo-all"
-
-echo "CREATE USER fms WITH PASSWORD '$DBPWD'; CREATE DATABASE fms WITH OWNER fms;" | su postgres -c "psql --echo-all"
-
-if ! echo "\q" | psql -d fms -U fms
-then
-    # psql: FATAL:  Ident authentication failed for user "fms"
-    echo "Adding 'local fms fms trust' to /etc/postgresql/8.4/main/pg_hba.conf"
-    cp /etc/postgresql/8.4/main/pg_hba.conf /etc/postgresql/8.4/main/pg_hba.conf.fms
-    echo "local fms fms trust\n" > /etc/postgresql/8.4/main/pg_hba.conf
-    cat /etc/postgresql/8.4/main/pg_hba.conf.fms >> /etc/postgresql/8.4/main/pg_hba.conf
-    rm /etc/postgresql/8.4/main/pg_hba.conf.fms
-    /etc/init.d/postgresql restart
-else
-    echo "Can connect to psql using fms"
-fi
-
-echo "CREATE LANGUAGE plpgsql;" | psql -d fms -U fms
-
-echo "Importing schemas"
-
-psql -d fms -U fms < db/schema.sql
-psql -d fms -U fms < db/alert_types.sql
-
-echo "Inserting secret"
-echo "INSERT INTO secret VALUES ('$RANDOMSALT');" | psql -d fms -U fms
+add-apt-repo
+setup-folders-git-checkout
+generate-locales
+install-and-setup-psql
 
 echo "Installing required packages"
 
@@ -130,7 +136,8 @@ cat ./conf/general.yml-example | sed\
 commonlib/bin/gettext-makemo FixMyStreet
 
 # missing module for admin/summary
-./bin/cron-wrapper ./local/bin/carton install Template::Plugin::DateTime::Format
+# only required for install w/o testing-repo
+#./bin/cron-wrapper ./local/bin/carton install Template::Plugin::DateTime::Format
 
 # missing module for app-dev-server "--restart"
 ./bin/cron-wrapper ./local/bin/carton install Catalyst::Restarter
